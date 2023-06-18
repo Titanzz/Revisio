aabc6758f73fb6914b264bc2af64c4824ed1da31
TechinTitan <74204487+TechinTitan@users.noreply.github.com>
2023-06-18 19:20:03 +0100

    Flashcard creation menu updates

--- main.py
+++ main.py
@@ -51,28 +51,43 @@ def createCards():
 
     flashcard_front_heading = tk.Label(menu_window, text="Flashcard Front", font=('Arial', 12, 'bold'))
     flashcard_front_heading.grid(row=0, column=0, sticky="w")
-
+    global flashcard_front_input
     flashcard_front_input = tk.Entry(menu_window)
     flashcard_front_input.grid(row=1, column=0, padx=10, pady=5)
 
     flashcard_back_heading = tk.Label(menu_window, text="Flashcard Back", font=('Arial', 12, 'bold'))
     flashcard_back_heading.grid(row=2, column=0, sticky="w")
-
+    global flashcard_back_input
     flashcard_back_input = tk.Entry(menu_window)
     flashcard_back_input.grid(row=3, column=0, padx=10, pady=5)
 
+    deck_label = tk.Label(menu_window, text="Select Deck", font=('Arial', 12, 'bold'))
+    deck_label.grid(row=5, column=0, sticky="w")
+    global deck_dropdown
+    deck_dropdown = tk.StringVar(menu_window)
+    deck_dropdown.set(decks[0][0])
+    deck_menu = tk.OptionMenu(menu_window, deck_dropdown, *decks)
+    deck_menu.grid(row=6, column=0, padx=10, pady=5)
+    
+
     add_flashcard_button = tk.Button(menu_window, text="Add Flashcard", command=add_flashcard)
     add_flashcard_button.grid(row=4, column=0, padx=10, pady=10)
-    
+
+
 def add_flashcard():
+    global flashcard_front_input
+    global flashcard_back_input
+    global deck_dropdown
     front_text = flashcard_front_input.get()
     back_text = flashcard_back_input.get()
+    selected_deck = deck_dropdown.get()
 
     flashcard_front_input.delete(0, tk.END)
     flashcard_back_input.delete(0, tk.END)
 
     print("Flashcard Front:", front_text)
     print("Flashcard Back:", back_text)
+    print("Selected Deck:", selected_deck)
 
 deckTitle = ttk.Label(root,text = "Main Menu",foreground = "black",font=('Nexa', 40))
 deckTitle.grid(row=0, column=2,padx=5, pady=5)
