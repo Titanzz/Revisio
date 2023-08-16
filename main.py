66ead8884ce5ad2ada07a641fe00c8cec0cc00d9
TechinTitan <74204487+TechinTitan@users.noreply.github.com>
2023-08-16 18:30:31 +0100

    Made the text resizable

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import simpledialog
 
 
 root = tk.Tk()
 root.title("Revisio")
-root.geometry("500x500")
-
+root.geometry("600x600")
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+def on_resize(event):
+    new_width = event.width
+    new_height = event.height
+    
+    for widget in root.winfo_children():
+        if isinstance(widget, tk.Button):
+            button_width = new_width // 6
+            button_height = new_height // 20
+            widget.config(width=button_width, height=button_height, font=("Helvetica", 10))
+        elif isinstance(widget, tk.Label) and widget == heading_label:
+            widget.config(font=("Helvetica", 20))
+root.bind("<Configure>", on_resize)
 
 decks = []
 
@@ -36,45 +51,49 @@ def close(window):
     window.destroy()
 
 def createNewDeck():
-    top = tk.Toplevel(root)
-    top.geometry("250x250")
-    top.title("New deck")
-    ttk.Label(top, text="Deck name", font=("Nexa", 15)).grid(padx=20, pady=20)
-    deckNameInput = tk.Entry(top)
-    deckNameInput.grid(padx=20, pady=22)
-    add = ttk.Button(top, text="Add", command = lambda:[addDeck(deckNameInput), close(top)]).grid(padx=20, pady=25)
+   top = tk.Toplevel(root)
+   top.geometry("250x250")
+   top.title("New deck")
+   label = tk.Label(top, text="Enter the deck name:", font=("Arial", 15))
+   label.pack(padx=20, pady=20)
+   entry = tk.Entry(top, font=("Arial", 15))
+   entry.pack(padx=20, pady=20)
+   add = ttk.Button(top, text="Add", command = lambda:[addDeck(entry), close(top)]).pack(padx=20, pady=20)
 
 def createCards():
     menu_window = tk.Toplevel()
+    menu_window.geometry("250x250")
+    menu_window.grid_rowconfigure(0, weight=1)
+    menu_window.grid_rowconfigure(1, weight=1)
+    menu_window.grid_rowconfigure(2, weight=1)
+    menu_window.grid_rowconfigure(3, weight=1)
+    menu_window.grid_rowconfigure(4, weight=1)
+    menu_window.grid_columnconfigure(0, weight=1)
 
     flashcard_front_heading = tk.Label(menu_window, text="Flashcard Front", font=('Arial', 12, 'bold'))
-    flashcard_front_heading.grid(row=0, column=0, sticky="w")
+    flashcard_front_heading.grid(row=0, column=0,)
     global flashcard_front_input
     flashcard_front_input = tk.Entry(menu_window)
-    flashcard_front_input.grid(row=1, column=0, padx=10, pady=5)
+    flashcard_front_input.grid(row=1, column=0)
 
     flashcard_back_heading = tk.Label(menu_window, text="Flashcard Back", font=('Arial', 12, 'bold'))
-    flashcard_back_heading.grid(row=2, column=0, sticky="w")
+    flashcard_back_heading.grid(row=2, column=0, )
     global flashcard_back_input
     flashcard_back_input = tk.Entry(menu_window)
-    flashcard_back_input.grid(row=3, column=0, padx=10, pady=5)
+    flashcard_back_input.grid(row=3, column=0)
 
     deck_label = tk.Label(menu_window, text="Select Deck", font=('Arial', 12, 'bold'))
-    deck_label.grid(row=5, column=0, sticky="w")
+    deck_label.grid(row=5, column=0, )
     global deck_dropdown
     deck_dropdown = tk.StringVar(menu_window)
     try:
         deck_dropdown.set(decks[0][0])
         deck_menu = tk.OptionMenu(menu_window, deck_dropdown, *decks)
-        deck_menu.grid(row=6, column=0, padx=10, pady=5)
+        deck_menu.grid(row=6, column=0)
         add_flashcard_button = tk.Button(menu_window, text="Add Flashcard", command=add_flashcard)
-        add_flashcard_button.grid(row=4, column=0, padx=10, pady=10)
+        add_flashcard_button.grid(row=4, column=0)
     except:
-            error = tk.Toplevel()
-            error.geometry("250x100")
-            error.title("Error message")
-            errormsg = tk.Label(error, text="Error, no decks to add flashcards into.").grid(padx=20, pady=20)
-            menu_window.destroy()
+            tk.messagebox.showerror("Error", "There are no decks to add cards to.")
 
 
 
@@ -95,14 +114,15 @@ def add_flashcard():
     print("Selected Deck:", selected_deck)
 
 deckTitle = ttk.Label(root,text = "Main Menu",foreground = "black",font=('Nexa', 40))
-deckTitle.grid(row=0, column=2,padx=5, pady=5)
+deckTitle.grid(row=0, column=0, columnspan=3, pady=20)
 
 newDeck = ttk.Button(root,text="New deck",command = createNewDeck)
 viewDeck = ttk.Button(root,text="View decks",command = viewDecks)
 createCard = ttk.Button(root,text="Create cards",command = createCards)
-viewDeck.grid(row=1, column=3, padx=5, pady=1, sticky='nsew')
-newDeck.grid(row=1, column=1, padx=5, pady=1, sticky='nsew')
-createCard.grid(row=1, column = 2, padx=5, pady=1, sticky='nsew')
+viewDeck.grid(row=1, column=2, padx=5, pady=1, sticky='nsew')
+newDeck.grid(row=1, column=0, padx=5, pady=1, sticky='nsew')
+createCard.grid(row=1, column = 1, padx=5, pady=1, sticky='nsew')
+
 
 root.mainloop()
 
